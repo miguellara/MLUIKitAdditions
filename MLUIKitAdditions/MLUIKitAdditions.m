e8fd53a7cc4e72294334a17796f970fcfefbcfee
Miguel Lara <miguel@mac.com>
2014-04-02 14:26:28 +0100

    Initial Commit

---
+++ MLUIKitAdditions/MLUIKitAdditions.m
@@ -0,0 +1,13 @@
+//
+//  MLUIKitAdditions.m
+//  MLUIKitAdditions
+//
+//  Created by Miguel Lara on 02/04/14.
+//  Copyright (c) 2014 Miguel Lara. All rights reserved.
+//
+
+#import "MLUIKitAdditions.h"
+
+@implementation MLUIKitAdditions
+
+@end
---
+++ MLUIKitAdditionsTests/MLUIKitAdditionsTests.m
@@ -0,0 +1,34 @@
+//
+//  MLUIKitAdditionsTests.m
+//  MLUIKitAdditionsTests
+//
+//  Created by Miguel Lara on 02/04/14.
+//  Copyright (c) 2014 Miguel Lara. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MLUIKitAdditionsTests : XCTestCase
+
+@end
+
+@implementation MLUIKitAdditionsTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
